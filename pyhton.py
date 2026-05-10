aaa5f6cbb1135581d0ed9e3802559c4888467677
nandi510 <90935402+nandi510@users.noreply.github.com>
2026-05-11 02:41:58 +0530

    Update pyhton.py

--- pyhton.py
+++ pyhton.py
@@ -1,1 +1,9 @@
-print('Hello World')  sdfasdf
+import random
+
+secret_number = random.randint(1, 10)
+guess = int(input("Guess a number between 1 and 10: "))
+
+if guess == secret_number:
+    print("Correct!")
+else:
+    print(f"Wrong! The number was {secret_number}.")
